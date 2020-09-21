2a96ccba241fcc2239c313c1c6b5142a4861d06f
lordjuacs <juacsr@gmail.com>
2020-09-21 12:17:53 -0500

    first session

---
+++ 1st/prueba.py
@@ -0,0 +1,24 @@
+#a = "123"
+#print(a)
+#print(type(a))
+
+#a = int(a)
+#print(a)
+#print(type(a))
+
+#v = "3.5"
+#print(float(v))
+
+#y = True
+#w = ""
+#w2 = "sakdnaslkdna"
+#print(bool(w))
+#print(bool(w2))
+
+#j = 0
+
+#print(bool(j))
+#DEFAULT CASE: input devuelve un STRING
+#n = float(input("Ingrese un numero: "))
+#print(type(n))
+print(round(19.62))
---
+++ 1st/test.py
@@ -0,0 +1,15 @@
+#int, 3, 589, 10000
+#float, 3.14159, 2.71
+#complex, 1 + 4i
+#bool, true false
+#str, "hola", "str"
+
+a = -34
+b = -3.14159
+c = False
+d = "Aasfasfas"
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
